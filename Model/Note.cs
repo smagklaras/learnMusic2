a3017174b352d89e9fe28bea112eca12c303e2a0
Stergios Magklaras <smagklaras@travix.com>
2020-04-29 17:19:49 +0200

    Added Notes class

---
+++ Model/Note.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace learnMusic.Model
+{
+    class Note
+    {
+        /// <summary>
+        /// the note name
+        /// </summary>
+        private readonly string name;
+
+        private decimal pitch;
+
+        /// <summary>
+        /// e.g. 440 HZ for middle A
+        /// </summary>
+        public decimal Pitch => pitch;
+
+        /// <summary>
+        /// the note name
+        /// </summary>
+        public string Name => name;
+
+        public Note(string name, decimal pitch)
+        {
+            this.name = name;
+            this.pitch = pitch;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using learnMusic.Model;
+
+
+namespace learnMusic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var myNote = new Note("G", 134.2m);
+            var myOtherNote = new Note("F", 240.3m);
+            
+            Console.WriteLine($"Hello World!");
+            Console.WriteLine($"This is my 1st note: {myNote.Name}, freq: {myNote.Pitch}");
+            Console.WriteLine($"This is my 2nd note: {myOtherNote.Name}, freq: {myOtherNote.Pitch}");
+        }
+    }
+}
